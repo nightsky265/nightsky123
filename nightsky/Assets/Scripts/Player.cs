a7a7dc53eaec84a4c67a05da0f2e5db41a89ec58
nighsky265 <56706540+nightsky265@users.noreply.github.com>
2019-11-08 12:00:18 +0800

    建立腳本定義欄位

---
+++ nightsky/Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("速度"), Range(0f, 100f)]
+    public float speed = 3.5f;
+    [Header("跳躍"), Range(100, 2000)]
+    public int jump = 300;
+    [Header("是否在地板上"), Tooltip("用來判定角色是否在地板上")]
+    public bool isGround = false;
+    [Header("角色名稱")]
+    public string name = "robot";
+}
