db50eea607e4c31d068e722ff890a26703b1aa9f
nighsky265 <56706540+nightsky265@users.noreply.github.com>
2019-12-06 11:58:02 +0800

    網頁版

--- nightsky/Assets/Scripts/Player.cs
+++ nightsky/Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;   // 引用 介面 API
 
 public class Player : MonoBehaviour
 {
@@ -21,6 +22,10 @@ public class Player : MonoBehaviour
     [Header("音效區域")]
     public AudioSource aud;
     public AudioClip soundDiamond;
+    [Header("鑽石區域")]
+    public int diamondCurrent;
+    public int diamondTotal;
+    public Text textDiamond;
     #endregion
     //定義方法
     //語法:
@@ -53,6 +58,14 @@ public class Player : MonoBehaviour
     {
 
     }
+    // 開始事件：播放時執行一次
+    private void Start()
+    {
+        // 鑽石總數 = 尋找所有指定標籤物件("指定標籤").數量
+        diamondTotal = GameObject.FindGameObjectsWithTag("鑽石").Length;
+        // 更新介面
+        textDiamond.text = "鑽石：0 / "+ diamondTotal;
+    }
 
     // 事件: 在特定時間點以指定次數執行
     // 更新事件: 一秒直行約60次(60FPS)
@@ -70,6 +83,17 @@ public class Player : MonoBehaviour
         if (collision.gameObject.name == "地板")
         {
             isGround = true;
+        }
+    }
+    //觸發事件：針對碰撞器有勾選 IsTrigger 的物件
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "鑽石")
+        {
+            aud.PlayOneShot(soundDiamond, 1.5f);    //音源.播放一次音效(音效，音量)
+            Destroy(collision.gameObject);          //刪除(碰撞的物件)
+            diamondCurrent++;                       //遞增
+            textDiamond.text = "鑽石：" + diamondCurrent + " / " + diamondTotal;
         }
     }
 }
